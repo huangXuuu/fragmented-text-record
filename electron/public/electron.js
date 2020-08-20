b7cad462a02c2f812fdfa03dc9db4fe9959c9608
黄旭 <huangxv@noahsi.com.cn>
2020-08-20 16:34:32 +0800

    限制只能打开一个窗口
    Closed 6

--- electron/public/electron.js
+++ electron/public/electron.js
@@ -36,7 +36,16 @@ const gotTheLock = app.requestSingleInstanceLock();
 if (!gotTheLock) {
   app.quit();
 } else {
-  app.on('second-instance', (event, argv, workingDirectory) => createWindow());
+  app.on('second-instance', (event, argv, workingDirectory) => {
+    // 当运行第二个实例时,将会聚焦到mainWindow这个窗口
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) {
+        mainWindow.restore();
+      }
+      mainWindow.focus();
+      mainWindow.show();
+    }
+  })
   app.on('ready', () => createWindow());
   if (process.platform === 'darwin') {
     app.on('before-quit', () => {
